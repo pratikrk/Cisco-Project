dde96a794cdf84d50ef3161c044d61f4a6132d55
pratikrk <83352530+pratikrk@users.noreply.github.com>
2022-11-15 00:31:23 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
-import * as React from 'react'
-import { Routes, Route, Navigate} from 'react-router-dom'
-import Home from './components/Home'
-import NavBar from './components/Navbar'
-import Team from './components/Team'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Footer, Gallery, HeroSection, TeamCard } from './containers';
+import { Insights, Navbar, TeamSection } from './components';
+import {  Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
 
+const Home = () => {
+  return (
+    <div className="App">
+      <div className='gradient__bg'>
+        <HeroSection />
+      </div>
+      <Insights />
+      <Gallery />
+      <Footer />
+    </div>
+  )
+}
 
-const App = () => {
+const About = () => {
   return (
     <div>
-      <NavBar />
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+const Courses = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+const Team = () => {
+  return (
+    <div>
+      <TeamCard/>
+    </div>
+  )
+}
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+// the components will be imported inside of containers
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/contactus" element={<ContactUs />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   )
 }
-export default App;
+
+export default App
---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// import ciscoLogo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="container mb-5">
+    <div className="row">
+      <div className="footer-col">
+        <h4>Links</h4>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Courses</a></li>
+          <li><a href="/">Team</a></li>
+          <li><a href="/">Contact Us</a></li>
+          
+        </ul>
+      </div>
+      <div className="footer-col">
+        <h4>Get In Touch</h4>
+        <ul>
+          <li><a href="/">kondhwa </a></li>
+          <li><a href="/">+918023</a></li>
+          <li><a href="/">ciscoviit@viit.ac.in</a></li>
+        </ul>
+      </div>
+      <div className="footer-col">
+        <h4>follow us</h4>
+        <div className="social-links">
+          <a href="/"><i className="fab fa-instagram"></i></a>
+          <a href="/"><i className="fab fa-linkedin-in"></i></a>
+          <a href="/"><i className="fab fa-facebook-f"></i></a>
+          <a href="/"><i className="fab fa-twitter"></i></a>
+        </div>
+      </div>
+      <div className="cisco__footer-copyright mb-3">
+        <p>@Cisco Team. All rights reserved.</p>
+      </div>
+    </div>
+   
+  </div>
+
+);
+
+export default Footer;
